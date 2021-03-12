6fb90649f8589f1735e1d561842b09bd1b72c6b0
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-03-12 17:23:25 +0900

    bug fix

--- converters/convert.py
+++ converters/convert.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Model
 from .tensorflow import tf_converter
 
 
-def convert(model, source='auto', inputs=None, outputs=None, classifier_config=None, minimum_deployment_target=None) -> ct.MLModel:
+def convert(model, source='auto', inputs=None, outputs=None, classifier_config=None, minimum_deployment_target=None) -> ct.models.MLModel:
 
     if isinstance(model, Model) or source == 'tensorflow':
         return tf_converter(model, inputs=inputs, outputs=outputs, classifier_config=classifier_config,

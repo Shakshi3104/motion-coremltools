18552b8f5559667a81a420866601a68e39fb1a7f
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-03-12 17:46:00 +0900

    Update documents

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # motion-coremltools
+
+motion-coremltools is the wrapper tool for neural networks with motion sensor data.
+
+## Usage
+The usage is the same as coremltools' Unified Conversation API. Currently, only tensorflow.keras.Model is supported.
+
+```python
+import coremltools as ct
+from coremotiontools import convert
+
+classifier_config = ct.ClassifierConfig(class_labels=["stay", "walk", "jog", "skip", "stUp", "stDown"])
+mlmodel = convert(model, classifier_config=classifier_config)
+```
+
+See [here](https://github.com/Shakshi3104/TF_CNN_Collection_to_CoreML/blob/master/main.py) for more detailed usages.
--- converters/convert.py
+++ converters/convert.py
@@ -5,7 +5,16 @@ from .tensorflow import tf_converter
 
 
 def convert(model, source='auto', inputs=None, outputs=None, classifier_config=None, minimum_deployment_target=None) -> ct.models.MLModel:
+    """Convert TensorFlow models for motion sensor data to the Core ML model format.
 
+    :param model: TensorFlow 2 model as tf.keras.Model
+    :param source: One of [`auto`, `tensorflow`, `pytorch`, `mil`]. `auto` determines the framework automatically for most cases.
+    :param inputs: list of `TensorType` or `ImageType`
+    :param outputs: list[str] (optional)
+    :param classifier_config: ClassifierConfig class (optional)
+    :param minimum_deployment_target: coremltools.target.enumeration (optional)
+    :return: coremltools.models.MLModel
+    """
     if isinstance(model, Model) or source == 'tensorflow':
         return tf_converter(model, inputs=inputs, outputs=outputs, classifier_config=classifier_config,
                             minimum_deployment_target=minimum_deployment_target)

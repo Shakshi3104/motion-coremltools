65dacfe46ef4ea08e57e91301d739b46aa9e0f62
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-03-12 17:21:15 +0900

    bug fix

--- converters/convert.py
+++ converters/convert.py
@@ -4,10 +4,10 @@ from tensorflow.keras.models import Model
 from .tensorflow import tf_converter
 
 
-def convert(model, source='auto', inputs=None, outputs=None, classifier_config=None, minimum_deployment_target=None):
+def convert(model, source='auto', inputs=None, outputs=None, classifier_config=None, minimum_deployment_target=None) -> ct.MLModel:
 
     if isinstance(model, Model) or source == 'tensorflow':
-        tf_converter(model, inputs=inputs, outputs=outputs, classifier_config=classifier_config,
-                     minimum_deployment_target=minimum_deployment_target)
+        return tf_converter(model, inputs=inputs, outputs=outputs, classifier_config=classifier_config,
+                            minimum_deployment_target=minimum_deployment_target)
     else:
         raise NotImplementedError()
